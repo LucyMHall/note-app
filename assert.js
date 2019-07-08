ffb39465e176f1941f64c368657e42f925a3a031
LucyMHall <lucymhall1995@gmail.com>
2019-07-08 17:04:40 +0100

    add a more comprehensive set of matchers with personalised error messages

---
+++ assert.js
@@ -0,0 +1,23 @@
+var assert = {
+  isInstanceOf: function(expected, actual) {
+    if (!(actual instanceof expected)) {
+      throw new Error("Assertion failed: " + (typeof actual) + " is not an instance of " + expected.name);
+    } else {
+      console.log("Passed: " + (typeof actual) + " is an instance of " + expected.name)
+    }
+  },
+  isEqual: function(expected, actual) {
+    if (!(expected == actual)) {
+      throw new Error("Failed " + expected + " is not equal to " + actual)
+    } else {
+      console.log("Passed " + expected + " is equal to " + actual)
+    }
+  },
+  isNotEqual: function(expected, actual) {
+    if ((expected == actual)) {
+      throw new Error("Failed " + expected + " is equal to " + actual)
+    } else {
+      console.log("Passed " + expected + " is not equal to " + actual)
+    }
+  }
+}
--- note-model-tests.js
+++ note-model-tests.js
@@ -3,9 +3,7 @@ console.log("Tests:");
 (function(exports) {
   function testNoteIsInstanceofNote() {
     var note = new Note();
-    if(!(note instanceof Note)) {
-      throw new Error("not an instance of Note");
-    }
+    assert.isInstanceOf(Note, note);
   };
   testNoteIsInstanceofNote();
 })(this);
