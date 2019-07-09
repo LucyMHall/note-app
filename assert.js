212c129d0515c20aa280f2e8acba249df0d44093
LucyMHall <lucymhall1995@gmail.com>
2019-07-09 12:36:38 +0100

    refactored errors to include try and catch syntax

--- assert.js
+++ assert.js
@@ -1,28 +1,48 @@
 var assert = {
   isInstanceOf: function(expected, actual) {
     if (!(actual instanceof expected)) {
-      throw new Error("Assertion failed: " + (typeof actual) + " is not an instance of " + expected.name);
+      try {
+        throw new Error("Assertion failed: " + (typeof actual) + " is not an instance of " + expected.name);
+      } catch (e) {
+        var stack = e.stack;
+        console.log(stack);
+      }
     } else {
       console.log("Passed: " + (typeof actual) + " is an instance of " + expected.name)
     }
   },
   isEqual: function(expected, actual) {
     if (!(expected == actual)) {
-      throw new Error("Failed " + expected + " is not equal to " + actual)
+      try {
+        throw new Error("Failed " + expected + " is not equal to " + actual)
+      } catch (e) {
+        var stack = e.stack;
+        console.log(stack);
+      }
     } else {
       console.log("Passed: " + expected + " is equal to " + actual)
     }
   },
   isNotEqual: function(expected, actual) {
     if ((expected == actual)) {
-      throw new Error("Failed " + expected + " is equal to " + actual)
+      try {
+        throw new Error("Failed " + expected + " is equal to " + actual)
+      } catch (e) {
+        var stack = e.stack;
+        console.log(stack);
+      }
     } else {
       console.log("Passed: " + expected + " is not equal to " + actual)
     }
   },
   includes: function(array, element) {
     if (!(array.includes(element))) {
-      throw new Error("Failed " + array + " does not include " + element)
+      try {
+        throw new Error("Failed " + array + " does not include " + element)
+      } catch (e) {
+        var stack = e.stack;
+        console.log(stack);
+      }
     } else {
       console.log("Passed: " + array + " does include " + element)
     }
