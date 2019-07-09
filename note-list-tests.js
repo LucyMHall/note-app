a33ad7b5c9b4a4fe90eb89595173a1a96d35ca3b
James Hylins <james.hylins@johnlewis.co.uk>
2019-07-09 11:28:46 +0100

    add a makeNote method to the NoteList

--- note-list-tests.js
+++ note-list-tests.js
@@ -9,5 +9,39 @@ console.log("Note List Tests:");
     assert.isEqual(noteList.listOfNotes.length, 0)
   }
 
-  test1()
+  console.log("makeNote")
+  function test2() {
+    describe("Should store an element in the list of notes")
+    var noteList = new NoteList();
+    noteList.makeNote("Example Text");
+    assert.isEqual(noteList.listOfNotes.length, 1);
+  }
+
+  function test3() {
+    describe("Should create an instance of Note")
+    var noteList = new NoteList();
+    noteList.makeNote("Example Text");
+    assert.isInstanceOf(Note, noteList.listOfNotes[0]);
+  }
+
+  function test4() {
+    describe("Should create an instance of Note with text property")
+    var noteList = new NoteList();
+    noteList.makeNote("Example Text");
+    assert.isEqual("Example Text", noteList.listOfNotes[0].text);
+  }
+
+  function test5() {
+    describe("Should append a new element to the existing list of notes")
+    var noteList = new NoteList();
+    noteList.makeNote("Example Text");
+    noteList.makeNote("Example Text 2");
+    assert.isEqual(noteList.listOfNotes.length, 2);
+  }
+
+  test1();
+  test2();
+  test3();
+  test4();
+  test5();
 })(this);
--- note-list.js
+++ note-list.js
@@ -2,5 +2,10 @@
   function NoteList() {
     this.listOfNotes = [];
   };
+
+  NoteList.prototype.makeNote = function(text, noteConstructor = Note) {
+    this.listOfNotes.push(new noteConstructor(text));
+  };
+
   exports.NoteList = NoteList;
 })(this);
