914425f310a4bc7dfa059a9b0ba1a688ac71154a
LucyMHall <lucymhall1995@gmail.com>
2019-07-11 12:20:40 +0100

    add a test for Note Controller display function which includes mocks for NoteList and NoteListView

---
+++ test/note-controller-test.js
@@ -0,0 +1,37 @@
+console.log("Note Controller Tests:");
+
+(function(exports) {
+
+  console.log("Contructor")
+
+  function test1() {
+    describe("instantiates a note controller")
+    return NoteController;
+    var aNoteController = new NoteController;
+    assert.isInstanceOf(NoteController, aNoteController);
+  };
+
+  console.log("display")
+
+  function test2() {
+    describe("changes innerHTML of app element")
+
+    function MockNoteList(){
+      this.makeNote = function(text) {};
+    }
+    function MockNoteListView(mockNoteList) {
+      this.displayNoteList = function() {
+        return "<ul><li><div>Favourite drink: seltzer</div></li></ul>"
+      }
+    }
+
+    var aNoteController = new NoteController(MockNoteList, MockNoteListView);
+    aNoteController.display()
+    element = document.getElementById("app")
+    assert.includes("<ul><li><div>Favourite drink: seltzer</div></li></ul>", element.innerHTML)
+  }
+
+  test1();
+  test2();
+
+})(this);
