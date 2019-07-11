a888b24b6ef72ab0be9cff4e83c8012539873c02
LucyMHall <lucymhall1995@gmail.com>
2019-07-11 11:19:50 +0100

    add a Note Controller which can display a hardcoded note on the index page

--- note-controller.js
+++ note-controller.js
@@ -1,4 +1,18 @@
+(function(exports) {
+  function NoteController(aNoteList = NoteList, aNoteListView = NoteListView) {
+    this.noteList = new aNoteList;
+    this.newNote = this.noteList.makeNote("Favourite drink: seltzer")
+    this.noteListView = new aNoteListView(this.noteList)
+  };
 
+  NoteController.prototype.display = function() {
     var element = document.getElementById("app");
-    console.log(element);
-    element.innerHTML = "Howdy"
+    element.innerHTML = this.noteListView.displayNoteList()
+  };
+
+  exports.NoteController = NoteController;
+
+})(this);
+
+noteController = new NoteController
+noteController.display();
